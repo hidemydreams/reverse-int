3e610d09d14d7134471658eb16ecf775baf875f4
Nazar Archakov <nazar.archakov222@gmail.com>
2021-02-09 14:05:28 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let positiveNum = Math.abs(n).toString().split("");
+    let newNum = [];
+
+    for (let i = 0; i < positiveNum.length; i++) {
+        newNum.unshift(positiveNum[i]);
+    }
+
+    return newNum.join("");
+};
